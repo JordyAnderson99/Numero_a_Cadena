ad9788c1444151719b7e3e79e1ba5c3a03751e09
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2020-01-25 00:17:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Numero_a_Cadena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            string valor = "";
+
+            Console.WriteLine("Digite el Numero");
+            valor = Console.ReadLine();
+            num = Convert.ToInt32(valor);
+
+            cadena();
+
+        }
+
+        static void cadena()
+        {
+            Console.WriteLine("Hola Mundo");
+
+        }
+
+    }
+}
